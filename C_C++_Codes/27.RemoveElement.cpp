10c644d24d5d732523439b6e90e2875cc9e292ae
Hardik Garg <hardiktechnoplanet@gmail.com>
2018-02-26 15:52:46 -0600

    Updated the code comments in line 5 to 7

--- C_C++_Codes/27.RemoveElement.cpp
+++ C_C++_Codes/27.RemoveElement.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 #include <iostream>
 using namespace std;
-// GENERATE PARENTHESES
-//Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+// REMOVE ELEMENT
+/*Given an array and a value, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.*/
 int RemoveElement(int* arr, int numsSize, int n) {
     
     int temp=0;
