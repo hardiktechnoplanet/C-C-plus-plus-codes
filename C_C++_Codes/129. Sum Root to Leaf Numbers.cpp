69760868bf36b5f68fd64ca0c5803090c9be08da
Hardik Garg <hardiktechnoplanet@gmail.com>
2020-01-09 21:33:09 -0500

    iterative method is added

--- C_C++_Codes/129. Sum Root to Leaf Numbers.cpp
+++ C_C++_Codes/129. Sum Root to Leaf Numbers.cpp
@@ -7,8 +7,10 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//Solution 1: Recursive
 //https://www.youtube.com/watch?v=bNgfwQspr10
 //call stack is also explanied in the video
+/*
 class Solution {
 public:
     int sumNumbers(TreeNode* root) 
@@ -32,4 +34,38 @@ public:
         
         return helper(root->left,pathSum)+helper(root->right,pathSum);
     }
+};
+*/
+
+//Solution 2: Iterative
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) 
+    {
+        //test case
+        if(root==NULL)
+            return 0;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,root->val});
+        int sum=0;
+        
+        while(!q.empty())
+        {
+            auto curr=q.front();
+            q.pop();
+            TreeNode* node=curr.first;
+            int val=curr.second;
+            
+            //if leaf node
+            if(node->left==NULL && node->right==NULL)
+                sum+=val;
+            
+            if(node->left)
+                q.push({node->left,val*10 + node->left->val});
+            if(node->right)
+                q.push({node->right,val*10 + node->right->val});
+        }
+        return sum;
+    }
+    
 };
