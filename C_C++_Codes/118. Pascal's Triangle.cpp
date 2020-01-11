5907526a192d6b40defba16f7d5fa1ce4dd67b5e
Hardik Garg <hardiktechnoplanet@gmail.com>
2020-01-10 22:33:41 -0500

    added 118. Pascal's Triangle

--- C_C++_Codes/118. Pascal's Triangle.cpp
+++ C_C++_Codes/118. Pascal's Triangle.cpp
@@ -6,6 +6,40 @@ public:
         if(numRows==0) 
             return vector<vector<int> >();
         
+        vector<vector<int>> result(numRows);
+        
+        // Iterate through every line
+        for(int line=0;line<numRows;++line)
+        {
+            // Every line has number of integers  
+            // equal to line number
+            for(int i=0;i<=line;++i)
+            {
+                //first and last value in every row is 1
+                if(line==i || i==0)
+                {
+                    result[line].push_back(1);
+                }
+                // Other values are sum of values just  
+                // above and left of above
+                else
+                { 
+                    result[line].push_back(result[line-1][i-1]+result[line-1][i]);
+                }
+            }
+        }
+        return result;
+        
+    }
+};
+/*class Solution {
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
+        //test case
+        if(numRows==0) 
+            return vector<vector<int> >();
+        
         vector<vector<int>> result(numRows, vector<int> (1,1));
         //first colum in result is filled with 1, so we dont have to 
         //fill the 0th row and 1st colum (for any row)
@@ -35,4 +69,4 @@ public:
         return result;
         
     }
-};
+};*/
