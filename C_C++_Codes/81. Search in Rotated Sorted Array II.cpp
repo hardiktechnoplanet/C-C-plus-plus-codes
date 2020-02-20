9325df74faf58efb7f4a2600b64b1ebb8c0661f2
Hardik Garg <hardiktechnoplanet@gmail.com>
2020-02-20 15:53:13 -0500

    added 81. Search in Rotated Sorted Array II

--- C_C++_Codes/81. Search in Rotated Sorted Array II.cpp
+++ C_C++_Codes/81. Search in Rotated Sorted Array II.cpp
@@ -1,79 +1,29 @@
-#include<bits/stdc++.h>
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/discuss/28218/My-8ms-C%2B%2B-solution-(o(logn)-on-average-o(n)-worst-case)
 
-using namespace std;
-//iterative method O(n)
-/*
-bool search (vector<int>& arr, int target)
-{
-	int n=arr.size();
-	//test case
-	if(n==0)
-	return false;
-	
-	for(int i=0;i<n;i++)
-	{
-		if(target==arr[i])
-		return true;
-	}
-	return false;
-}
-*/
-//binary search O(logn)
-bool search(vector<int>& arr, int target)
-{
-	int n=arr.size();
-	if(n==0)
-	{
-		return 0;
-	}
-	int left=0, right=n-1;
-	
-	while(left<=right)
-	{
-		int mid=(left+right)/2;
-		if(arr[mid]==target)
-		return true;
-		
-		//mid is the pivot
-		//mid is the pivot implies it the largers element in the array
-		else if(arr[mid]>arr[right])
-		{
-			if(arr[left]<=target && arr[mid]>=target)
-			right=mid-1;
-			else
-			left=mid+1;
-		}
-		
-		else if(arr[mid]<arr[right])
-		{
-			if(arr[mid]<=target && arr[right]>=target)
-			left=mid+1;
-			else
-			right=mid-1;
-		}
-		else
-		right--;
-	}
-	return false;
-}
-int main()
-{
-	vector<int> arr;
-	arr.push_back(2);
-	arr.push_back(5);
-	arr.push_back(6);
-	arr.push_back(0);
-	arr.push_back(0);
-	arr.push_back(1);
-	
-	if(search(arr,3)==true)
-	{
-		cout<<"target is present in the vector"<<endl;
-	}
-	else
-	{
-		cout<<"target is not present in the vector"<<endl;
-	}
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right =  nums.size()-1, mid;
+        
+        while(left<=right)
+        {
+            mid = (left + right) >> 1;
+            if(nums[mid] == target) return true;
 
-	return 0;
-}
+            // the only difference from the first one, trickly case, just updat left and right
+            if( (nums[left] == nums[mid]) && (nums[right] == nums[mid]) ) {++left; --right;}
+
+            else if(nums[left] <= nums[mid])
+            {
+                if( (nums[left]<=target) && (nums[mid] > target) ) right = mid-1;
+                else left = mid + 1; 
+            }
+            else
+            {
+                if((nums[mid] < target) &&  (nums[right] >= target) ) left = mid+1;
+                else right = mid-1;
+            }
+        }
+        return false;
+    }
+};
